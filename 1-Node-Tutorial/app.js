0972a55ad6bef91b2c2613ebd3d1e99615981837
Naveen-Kumar45 <naveenkumarreddy2110@gmail.com>
2026-06-12 19:13:07 +0530

    Addded  Node tutorial files correctly

---
+++ 1-Node-Tutorial/app.js
@@ -0,0 +1,13 @@
+/* lodash is a JavaScript library that provides utility functions for common programming tasks, such as manipulating arrays, objects, and functions. It is designed to be fast and efficient, and it can be used in both client-side and server-side applications. 
+Some of the features of lodash include:
+- Array manipulation: Lodash provides a wide range of functions for working with arrays, such as filtering, mapping, reducing, and sorting.
+- Object manipulation: Lodash offers functions for working with objects, including merging, cloning, and picking specific properties.
+- Function utilities: Lodash includes functions for working with functions, such as debouncing, throttling, and currying.
+- String manipulation: Lodash provides functions for working with strings, such as trimming, padding, and escaping.
+- Utility functions: Lodash includes various utility functions for tasks like type checking, deep equality comparison, and random number generation. */
+
+
+
+
+
+console.log("Happy for visiting my repository and learning Node.js with me :)")
