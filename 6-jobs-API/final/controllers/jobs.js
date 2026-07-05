50c6eed878e5a44177c88c246c6b89c630b2b561
Naveen-Kumar45 <naveenkumarreddy2110@gmail.com>
2026-07-05 22:21:55 +0530

    Update Error Handling

--- 6-jobs-API/final/controllers/jobs.js
+++ 6-jobs-API/final/controllers/jobs.js
@@ -9,6 +9,7 @@ const getAllJobs = async(req,res) => {
 const getJob = async (req,res) => {
     //console.log(req.params.id)
     //console.log(req.user.userId)
+    //console.log(req.user)
     const jobId = req.params.id 
     const userId = req.user.userId
     const job = await Job.findOne({
@@ -16,7 +17,11 @@ const getJob = async (req,res) => {
         createdBy : userId
     })
 
-    res.status(200).json(job)
+    if (!job){
+        throw createError(404, `No Job with the ID : ${jobId}`)
+    }
+
+    res.status(200).json({job})
 }
 
 const postJob = async(req,res) => {
@@ -36,17 +41,17 @@ const updateJob = async (req,res) => {
         body : {company,position}
     } = req
 
-    if ( company === "" || position === ""){
+    if ( company.trim() === "" || position.trim() === ""){
         throw createError.BadRequest("Company and position fields cannot be empty")
     }
 
     const job = await Job.findByIdAndUpdate({_id : jobId, createdBy : userId}, req.body, { returnDocument : 'after', runValidators : true})
 
     if (!job){
-        throw createError(401, `No Job with the ID ${jobId}`)
+        throw createError(404, `No Job with the ID ${jobId}`)
     }
 
-    res.status(201).json(job)
+    res.status(201).json({job})
 }
 
 const deleteJob = async (req,res) => {
@@ -58,10 +63,10 @@ const deleteJob = async (req,res) => {
     const job = await Job.findByIdAndDelete({_id : jobId, createdBy : userId})
 
     if (!job){
-        throw createError.BadRequest(`No Job with the ID ${jobId}`)
+        throw createError(404, `No Job with the ID ${jobId}`)
     }
 
-    res.status(200).json(job)
+    res.status(200).json({job})
 }
 
 
