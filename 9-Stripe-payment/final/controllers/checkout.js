2488c1f8f7c7a6944bdc49805ef016f055f4ae38
Naveen-Kumar45 <naveenkumarreddy2110@gmail.com>
2026-08-13 02:58:03 +0530

    Removed git from local

--- 9-Stripe-payment/final/controllers/checkout.js
+++ 9-Stripe-payment/final/controllers/checkout.js
@@ -17,8 +17,30 @@ const Order = require("../models/orders")
 const createRazorpayCheckout = async (req,res) => {
     const productIDs = req.body.items.map(item => item.productId)
 
-    const products = Product.find({
+    const products = await Product.find({
         _id : { $in : productIDs}
+    })
+
+    //console.log(products)
+    total_amount=0
+    
+    for (let item of req.body.items){
+
+        const product = products.find(product => product._id.toString() === item.productId)
+
+        total_amount+=product.price * item.quantity
+    }
+
+    const order = await razorpay.orders.create({
+        amount : total_amount,
+        currency : "INR",
+        receipt : `receipt_${Date.now()}`
+    })
+
+    console.log(order)
+
+    res.status(200).json({
+        order
     })
 }
 
